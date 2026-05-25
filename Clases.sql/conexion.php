08f7842114d46cfe6712418bb933bd204a1c098b
Marlene <c22160987@voaxaca.tecnm.mx>
2026-05-24 21:18:14 -0600

    Add Conexion class for database connection

---
+++ Clases.sql/conexion.php
@@ -0,0 +1,32 @@
+<?php
+
+class Conexion {
+    private $host = "localhost";
+    private $usuario = "root";
+    private $password = "";
+    private $bd = "aerolinea";
+
+    public $conexion;
+
+    public function __construct() {
+        $this->conexion = new mysqli(
+            $this->host,
+            $this->usuario,
+            $this->password,
+            $this->bd
+        );
+
+        if ($this->conexion->connect_error) {
+            die("Error de conexión: " . $this->conexion->connect_error);
+        }
+    }
+
+    public function ejecutar($sql) {
+        return $this->conexion->query($sql);
+    }
+
+    public function consultar($sql) {
+        return $this->conexion->query($sql);
+    }
+}
+?>
